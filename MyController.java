0bf3dabb37b324a2ff9742df7803d1afe1bbfc0e
lorefior <56115031+lorefior@users.noreply.github.com>
2019-10-26 17:21:31 -0300

    cambio

--- MyController.java
+++ MyController.java
@@ -6,25 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.calculo.neto.AppNeto;
+import com.calculo.neto.Greeting;
 
 @Controller
-public class MyController {
+public class MyController{
 
-	@GetMapping("/sueldoNeto")
-	public String sueldoNetoForm(Model model) {
-		model.addAttribute("sueldoNeto", new AppNeto());
-		return "sueldoNeto";
-	}
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
 
-	@PostMapping("/sueldoNeto")
-	public String sueldoNetoSubmit(@ModelAttribute AppNeto sueldoNeto, double sueldo, double jubilacion, double pami, double obraSocial) {
-		sueldoNeto.pamiCal(sueldo);
-		sueldoNeto.jubCal(sueldo);
-		sueldoNeto.netoCal(sueldo, jubilacion, pami, obraSocial);
-		sueldoNeto.obraCal(sueldo);
-
-		return "sueldoNeto";
-	}
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, float sueldo) {
+    	greeting.juilacion(sueldo);
+    	greeting.obraSocial(sueldo);
+    	greeting.pamiley(sueldo);
+    	greeting.sueldoneto(sueldo);
+        return "greeting";
+    }
 
 }
